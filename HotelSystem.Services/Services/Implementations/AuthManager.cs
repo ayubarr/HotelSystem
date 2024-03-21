3e0dc1aa912fbc10571a2879c151c9aa7382959a
ayub <ayub.arbiev@gmail.com>
2024-03-21 20:04:15 +0600

    models

---
+++ HotelSystem.Services/Services/Implementations/AuthManager.cs
@@ -0,0 +1,15 @@
+﻿using HotelSystem.Domain.Models.Abstractions.BaseEntities;
+using HotelSystem.Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSystem.Services.Services.Implementations
+{
+    public class AuthManager<T> : IAuthManager<T>
+        where T : ApplicationUser
+    {
+    }
+}
