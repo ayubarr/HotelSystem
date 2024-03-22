d7f6877ad55d5a568dcad64e003d3d761b63ac8e
Sneaks <dimos.yaropolov@mail.ru>
2024-03-22 19:08:13 +0300

    add i don't know what i write

--- HotelSystem.Services/Services/Implementations/AuthManager.cs
+++ HotelSystem.Services/Services/Implementations/AuthManager.cs
@@ -1,8 +1,12 @@
 ﻿using HotelSystem.ApiModels.Auth;
 using HotelSystem.ApiModels.Response.Interfaces;
 using HotelSystem.Domain.Models.Abstractions.BaseEntities;
+using HotelSystem.Domain.Models.Entities;
 using HotelSystem.Services.Services.Interfaces;
+using HotelSystem.Validation;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -16,6 +20,22 @@ namespace HotelSystem.Services.Services.Implementations
 	public class AuthManager<T> : IAuthManager<T>, ITokenManager<T>
 		where T : ApplicationUser
 	{
+		private readonly UserManager<Employee> _userManager;
+
+		private readonly IConfiguration _configuration;
+
+		public AuthManager(UserManager<Employee> userManager,
+			IConfiguration configuration)
+		{
+			ObjectValidator<UserManager<Employee>>.CheckIsNotNullObject(userManager);
+			_userManager = userManager;
+
+			ObjectValidator<IConfiguration>.CheckIsNotNullObject(configuration);
+			_configuration = configuration;
+		}
+
+
+
 		public Task<IBaseResponse<AuthResultStruct>> Login(LoginModel model)
 		{
 			throw new NotImplementedException();
@@ -36,9 +56,22 @@ namespace HotelSystem.Services.Services.Implementations
 			throw new NotImplementedException();
 		}
 
-		JwtSecurityToken ITokenManager<T>.GenerateToken(List<Claim> claims)
+		JwtSecurityToken ITokenManager<T>.GenerateToken(List<Claim> authClaims)
 		{
-			throw new NotImplementedException();
+			ObjectValidator<List<Claim>>.CheckIsNotNullObject(authClaims);
+
+			var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+			bool isTokenValidityInt = int.TryParse(_configuration["JWT:TokenValidityInMinutes"], out int tokenValidityInMinutes);
+
+			return new JwtSecurityToken(
+					issuer: _configuration["JWT:ValidIssuer"],
+					audience: _configuration["JWT:ValidAudience"],
+					expires: DateTime.Now.AddMinutes(isTokenValidityInt ? tokenValidityInMinutes : 0),
+					claims: authClaims,
+					signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+					);
+
+
 		}
 
 		ClaimsPrincipal ITokenManager<T>.GetClaimsPrincipalFromExpiredToken(string? token)
