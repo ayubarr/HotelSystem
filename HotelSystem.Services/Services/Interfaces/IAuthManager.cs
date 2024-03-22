fd8221a4a4bd9829267462e2e96be341b3c52b30
ayub <ayub.arbiev@gmail.com>
2024-03-22 21:20:17 +0600

    add documentations

--- HotelSystem.Services/Services/Interfaces/IAuthManager.cs
+++ HotelSystem.Services/Services/Interfaces/IAuthManager.cs
@@ -4,12 +4,25 @@ using HotelSystem.Domain.Models.Abstractions.BaseEntities;
 
 namespace HotelSystem.Services.Services.Interfaces
 {
-    public interface IAuthManager<Tmodel> 
-        where Tmodel : ApplicationUser
+    /// <summary>
+    /// Определяет методы для управления аутентификацией и авторизацией пользователей.
+    /// </summary>
+    /// <typeparam name="Tmodel">Тип модели пользователя, наследующей ApplicationUser.</typeparam>
+    public interface IAuthManager<Tmodel> where Tmodel : ApplicationUser
     {
-        public Task<IBaseResponse<AuthResultStruct>> Login(LoginModel model);
+        /// <summary>
+        /// Выполняет процесс входа пользователя в систему.
+        /// </summary>
+        /// <param name="model">Модель, содержащая учетные данные для входа.</param>
+        /// <returns>Объект, представляющий результат операции аутентификации.</returns>
+        Task<IBaseResponse<AuthResultStruct>> Login(LoginModel model);
 
-        public Task<IBaseResponse<uint>> Register(RegisterModel model);
+        /// <summary>
+        /// Выполняет процесс регистрации нового пользователя.
+        /// </summary>
+        /// <param name="model">Модель, содержащая данные нового пользователя для регистрации.</param>
+        /// <returns>Объект, представляющий результат операции регистрации.</returns>
+        Task<IBaseResponse<uint>> Register(RegisterModel model);
+    }
 
-	}
 }
