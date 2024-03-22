1bfb1d67092eba0722551f0aeb09cbf9c31128fd
Sneaks <dimos.yaropolov@mail.ru>
2024-03-22 18:21:26 +0300

    Implement ITokenManager

--- HotelSystem.Services/Services/Implementations/AuthManager.cs
+++ HotelSystem.Services/Services/Implementations/AuthManager.cs
@@ -5,13 +5,15 @@ using HotelSystem.Services.Services.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HotelSystem.Services.Services.Implementations
 {
-	public class AuthManager<T> : IAuthManager<T>
+	public class AuthManager<T> : IAuthManager<T>, ITokenManager<T>
 		where T : ApplicationUser
 	{
 		public Task<IBaseResponse<AuthResultStruct>> Login(LoginModel model)
@@ -20,6 +22,31 @@ namespace HotelSystem.Services.Services.Implementations
 		}
 
 		public Task<IBaseResponse<uint>> Register(RegisterModel model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IBaseResponse<AuthResultStruct>> UpdateToken(TokenModel model)
+		{
+			throw new NotImplementedException();
+		}
+
+		string ITokenManager<T>.GenerateRefreshToken()
+		{
+			throw new NotImplementedException();
+		}
+
+		JwtSecurityToken ITokenManager<T>.GenerateToken(List<Claim> claims)
+		{
+			throw new NotImplementedException();
+		}
+
+		ClaimsPrincipal ITokenManager<T>.GetClaimsPrincipalFromExpiredToken(string? token)
+		{
+			throw new NotImplementedException();
+		}
+
+		Task<IBaseResponse<AuthResultStruct>> ITokenManager<T>.UpdateToken(TokenModel model)
 		{
 			throw new NotImplementedException();
 		}
