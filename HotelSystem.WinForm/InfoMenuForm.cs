623e7fe53590de6034352c71a24cbf4734bba0ce
ayub <ayub.arbiev@gmail.com>
2024-05-07 16:54:28 +0600

    add info menu

--- HotelSystem.WinForm/InfoMenuForm.cs
+++ HotelSystem.WinForm/InfoMenuForm.cs
@@ -5,126 +5,95 @@ using System.Data;
 
 namespace HotelSystem.WinForm
 {
-	public partial class InfoMenuForm : Form
-	{
-		public InfoMenuForm()
-		{
-			InitializeComponent();
-		}
+    public partial class InfoMenuForm : Form
+    {
+        public InfoMenuForm()
+        {
+            InitializeComponent();
+        }
 
-		private void button_back_click(object sender, EventArgs e)
-		{
-			var mainMenu = new MainMenu();
-			mainMenu.Show();
-			Visible = false;
-		}
+        private void button_back_click(object sender, EventArgs e)
+        {
+            var mainMenu = new MainMenu();
+            mainMenu.Show();
+            Visible = false;
+        }
 
-		private void label_Prolongation_Click(object sender, EventArgs e)
-		{
+        private void label_Prolongation_Click(object sender, EventArgs e)
+        {
 
-		}
+        }
 
-		private void label_GetInformation_Click(object sender, EventArgs e)
-		{
+        private void label_GetInformation_Click(object sender, EventArgs e)
+        {
 
-		}
+        }
 
-		private void button_InfoPayments_Click(object sender, EventArgs e)
-		{
+        private void button_InfoPayments_Click(object sender, EventArgs e)
+        {
 
-		}
+        }
 
-		private void button_InfoClients_Click(object sender, EventArgs e)
-		{
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
-		}
+        }
 
-		private async void button_InfoRooms_Click(object sender, EventArgs e)
-		{
-			//if (!CheckLoginAndPassword())
-			//{
-			//	label_GuestInputError.Visible = true;
-			//	return;
-			//}
-			//string jsonContent = JsonConvert.SerializeObject(new
-			//{
-			//	FirstName = listBox1.Text,
-			//});
-			try
-			{
-				using (HttpClient httpClient = new HttpClient())
-				{
-					httpClient.DefaultRequestHeaders.Add("accept", "*/*");
+        private async void button_InfoGuest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    httpClient.DefaultRequestHeaders.Add("accept", "*/*");
 
-					var response = await httpClient.GetAsync("https://localhost:5001/api/Guest/GetGuests");
+                    var response = await httpClient.GetAsync("https://localhost:5001/api/Guest/GetGuests");
 
-					// Проверяем успешность запроса
-					if (!response.IsSuccessStatusCode)
-					{
-						throw new Exception(response.RequestMessage.ToString());
-					}
-					var json = await response.Content.ReadAsStringAsync();
-					var guests = JsonConvert.DeserializeObject<IEnumerable<Guest>>(json);
+                    // Проверяем успешность запроса
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        throw new Exception(response.RequestMessage.ToString());
+                    }
+                    var json = await response.Content.ReadAsStringAsync();
+                    var guests = JsonConvert.DeserializeObject<IEnumerable<Guest>>(json);
 
                     foreach (var guest in guests)
-					{
-						string res = guest.MiddleName + "\t\t" + guest.FirstName + "\t\t" + guest.LastName;
-						listBox1.Items.Add(res);
-						//Console.WriteLine(guest.FirstName); 
-					}
-				}
+                    {
+                        string res = guest.MiddleName + "\t\t" + guest.FirstName + "\t\t" + guest.LastName;
+                        listBox1.Items.Add(res);
+                        //Console.WriteLine(guest.FirstName); 
+                    }
+                }
 
-			}
-			catch (Exception ex)
-			{
-				// Обрабатываем исключение
-				MessageBox.Show($"При добавлении нового гостя возникла ошибка: \n\r{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-			}
-		}
+            }
+            catch (Exception ex)
+            {
+                // Обрабатываем исключение
+                MessageBox.Show($"При добавлении нового гостя возникла ошибка: \n\r{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
 
-		private void InfoMenuForm_FormClosing(object sender, FormClosingEventArgs e)
-		{
-			Environment.Exit(0);
-		}
+        private async void CreateDataGuestTable(IEnumerable<Guest>? guests)
+        {
+            // Создаем DataTable и добавляем в него данные
+            DataTable dTGuest = new DataTable();
 
-		private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-		{
+            dTGuest.Columns.Add("Фамилия");
+            dTGuest.Columns.Add("Имя");
+            dTGuest.Columns.Add("Отчество");
+            foreach (var guest in guests)
+            {
 
-		}
+                dTGuest.Rows.Add(guest.MiddleName, guest.FirstName, guest.LastName);
+            }
 
+            // Связываем DataGridView с DataTable
+            dataGridView1.DataSource = dTGuest;
+        }
 
-		//private bool CheckLoginAndPassword()
-		//{
-		//	if (string.IsNullOrEmpty(textBox_FirstName.Text))
-		//	{
-		//		textBox_FirstName.BackColor = Color.FromArgb(255, 192, 192);
-		//		return false;
-		//	}
-		//	if (string.IsNullOrEmpty(textBox_LastName.Text))
-		//	{
-		//		textBox_LastName.BackColor = Color.FromArgb(255, 192, 192);
-		//		return false;
-		//	}
-		//	if (string.IsNullOrEmpty(textBox_MiddleName.Text))
-		//	{
-		//		textBox_MiddleName.BackColor = Color.FromArgb(255, 192, 192);
-		//		return false;
-		//	}
-		//	if (dateTimePicker_RegistrationStartDate.Value < dateTimePicker_RegistrationEndDate.Value)
-		//	{
-		//		MessageBox.Show($"Дата начала регистрации не может быть раньше даты окончания регистрации", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-		//		return false;
-		//	}
-		//	if (string.IsNullOrEmpty(textBox_Email.Text))
-		//	{
-		//		textBox_Email.Text = "example@mail.com";
-		//	}
-		//	if (string.IsNullOrEmpty(textBox_PhoneNumber.Text))
-		//	{
-		//		textBox_PhoneNumber.Text = "+XXX-XXX-XXX";
-		//	}
-
-		//	return true;
-		//}
-	}
+        private void InfoMenuForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
 }
