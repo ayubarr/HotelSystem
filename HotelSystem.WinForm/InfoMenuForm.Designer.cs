c3246f6020e218a7a5da1f82817c0912ca455792
Sneaks <dimos.yaropolov@mail.ru>
2024-05-07 13:42:39 +0300

    add CreateDataGuestTable

--- HotelSystem.WinForm/InfoMenuForm.Designer.cs
+++ HotelSystem.WinForm/InfoMenuForm.Designer.cs
@@ -22,6 +22,130 @@
 
         #region Windows Form Designer generated code
 
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			label_MainMenu = new Label();
+			button_back = new Button();
+			label_GetInformation = new Label();
+			button_GetInformation = new Button();
+			label_Reservation = new Label();
+			button_InfoGuest = new Button();
+			listBox1 = new ListBox();
+			dataGridView1 = new DataGridView();
+			((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+			SuspendLayout();
+			// 
+			// label_MainMenu
+			// 
+			label_MainMenu.AutoSize = true;
+			label_MainMenu.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+			label_MainMenu.Location = new Point(302, 9);
+			label_MainMenu.Name = "label_MainMenu";
+			label_MainMenu.Size = new Size(419, 37);
+			label_MainMenu.TabIndex = 3;
+			label_MainMenu.Text = "Меню Для Поиска Информации";
+			// 
+			// button_back
+			// 
+			button_back.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+			button_back.Location = new Point(900, 609);
+			button_back.Name = "button_back";
+			button_back.Size = new Size(152, 60);
+			button_back.TabIndex = 19;
+			button_back.Text = "Вернуться назад";
+			button_back.UseVisualStyleBackColor = true;
+			button_back.Click += button_back_click;
+			// 
+			// label_GetInformation
+			// 
+			label_GetInformation.AutoSize = true;
+			label_GetInformation.FlatStyle = FlatStyle.System;
+			label_GetInformation.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+			label_GetInformation.Location = new Point(742, 264);
+			label_GetInformation.Name = "label_GetInformation";
+			label_GetInformation.Size = new Size(259, 25);
+			label_GetInformation.TabIndex = 27;
+			label_GetInformation.Text = "Информация о Сотрудниках";
+			label_GetInformation.Click += label_GetInformation_Click;
+			// 
+			// button_GetInformation
+			// 
+			button_GetInformation.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			button_GetInformation.Location = new Point(661, 290);
+			button_GetInformation.Margin = new Padding(3, 2, 3, 2);
+			button_GetInformation.Name = "button_GetInformation";
+			button_GetInformation.Size = new Size(360, 38);
+			button_GetInformation.TabIndex = 26;
+			button_GetInformation.Text = "Информация о Сотрудниках";
+			button_GetInformation.UseVisualStyleBackColor = true;
+			// 
+			// label_Reservation
+			// 
+			label_Reservation.AutoSize = true;
+			label_Reservation.FlatStyle = FlatStyle.System;
+			label_Reservation.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+			label_Reservation.Location = new Point(101, 303);
+			label_Reservation.Name = "label_Reservation";
+			label_Reservation.Size = new Size(226, 25);
+			label_Reservation.TabIndex = 21;
+			label_Reservation.Text = "Информация о клиентах";
+			// 
+			// button_InfoGuest
+			// 
+			button_InfoGuest.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			button_InfoGuest.Location = new Point(40, 330);
+			button_InfoGuest.Margin = new Padding(3, 2, 3, 2);
+			button_InfoGuest.Name = "button_InfoGuest";
+			button_InfoGuest.Size = new Size(360, 38);
+			button_InfoGuest.TabIndex = 20;
+			button_InfoGuest.Text = "Информация о клиентах";
+			button_InfoGuest.UseVisualStyleBackColor = true;
+			button_InfoGuest.Click += button_InfoGuest_Click;
+			// 
+			// listBox1
+			// 
+			listBox1.FormattingEnabled = true;
+			listBox1.ItemHeight = 15;
+			listBox1.Items.AddRange(new object[] { "Фамилия\t\tИмя\t\tОтчество" });
+			listBox1.Location = new Point(22, 89);
+			listBox1.Name = "listBox1";
+			listBox1.Size = new Size(425, 199);
+			listBox1.TabIndex = 28;
+			// 
+			// dataGridView1
+			// 
+			dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dataGridView1.Location = new Point(40, 404);
+			dataGridView1.Name = "dataGridView1";
+			dataGridView1.RowTemplate.Height = 25;
+			dataGridView1.Size = new Size(360, 175);
+			dataGridView1.TabIndex = 29;
+			dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+			// 
+			// InfoMenuForm
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(1064, 681);
+			Controls.Add(dataGridView1);
+			Controls.Add(listBox1);
+			Controls.Add(label_GetInformation);
+			Controls.Add(button_GetInformation);
+			Controls.Add(label_Reservation);
+			Controls.Add(button_InfoGuest);
+			Controls.Add(button_back);
+			Controls.Add(label_MainMenu);
+			Name = "InfoMenuForm";
+			Text = "InfoMenuForm";
+			FormClosing += InfoMenuForm_FormClosing;
+			((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
         /// <summary>
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
@@ -229,16 +353,9 @@
 		private Button button_back;
 		private Label label_GetInformation;
 		private Button button_GetInformation;
-		private Label label_Prolongation;
-		private Button button_InfoRooms;
-		private Label label_ClientRegistration;
-		private Button button_InfoPayments;
 		private Label label_Reservation;
-		private Button button_InfoClients;
+		private Button button_InfoGuest;
 		private ListBox listBox1;
 		private DataGridView dataGridView1;
-		private DataGridViewTextBoxColumn Фамилия;
-		private DataGridViewTextBoxColumn Имя;
-		private DataGridViewTextBoxColumn Отчество;
 	}
 }
